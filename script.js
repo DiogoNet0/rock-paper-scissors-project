c118252a21cb18207d7d4f8ed2368b37a7aa6a3f
DiogoNet0 <diogoneto1779@gmail.com>
2024-06-04 14:18:46 +0100

    Wrote a way to get a random computer choice to rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+let getComputerChoice = Math.floor(Math.random() * 3);
+let computerString;
+
+if (getComputerChoice === 0) {
+  computerString = "Rock!";
+} else if (getComputerChoice === 1) {
+  computerString = "Paper!";
+} else {
+  computerString = "Scissors!";
+}
+alert(computerString);
+
+let getHumanChoice = prompt("Rock, Paper or Scissors?", "");
