a96c68e9e8d3e7f2ace99b9b06d1f74ca59d8ed4
DiogoNet0 <diogoneto1779@gmail.com>
2024-06-05 22:50:39 +0100

    Created a function that gets the player's and computer's choices and compares the results and outputs if the player won or loss

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let humanScore = 0;
-let computerScore = 0;
+let playerScore = 0; // Keeps track of Player´ score
+let computerScore = 0; // Keeps track of Computer´ score
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
-}
+} // Randomly generates a choice for the game
 
-function getHumanChoice() {
+function getPlayerChoice() {
   const userInput = prompt(
     "Enter your choice (rock, paper, scissors): "
   ).toLowerCase();
@@ -20,13 +20,36 @@ function getHumanChoice() {
     return userInput;
   } else {
     console.log("Invalid choice! Please enter rock, paper, or scissors.");
-    return getHumanChoice();
+    return getPlayerChoice();
   }
-}
+} // Gets an input from the player and checks if its valid or not
 
-const humanChoice = getHumanChoice();
+const playerChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
-console.log(humanChoice);
-console.log(computerChoice);
+console.log("You chose: ", playerChoice);
+console.log("The Computer chose: ", computerChoice);
 
-function playRound(humanChoice, computerChoice) {}
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "It's a draw!";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    playerScore = +1;
+    return "You Win! Rock beats Scissors!";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    playerScore = +1;
+    return "You Win! Scissors beats Paper!";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    playerScore = +1;
+    return "You Win! Paper beats Rock";
+  } else {
+    computerScore = +1;
+    return "You Lost!";
+  }
+} //
+
+const playResult = playRound(playerChoice, computerChoice);
+alert(playResult);
+console.log("Player - ", playerScore);
+console.log("Computer - ", computerScore);
+
+for (i = 0; i <= 5; i++) {}
