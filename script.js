3befefa7aa710e84af3df2d38ceab4ee33cce735
DiogoNet0 <diogoneto1779@gmail.com>
2024-06-07 11:12:01 +0100

    Applied style to the console messages

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
+  // Styles for Win/Loss Message
+  const PLAYER_LETTER = "font-size:20px; color: green;";
+  const COMPUTER_LETTER = "font-size:20px; color: red;";
+
   // Randomly generates a choice for the game
   function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -28,8 +32,7 @@ function playGame() {
     }
   }
 
-  // converts the functions to variables
-
+  // Converts the functions to variables
   function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
       return "It's a draw!";
@@ -54,18 +57,18 @@ function playGame() {
     const computerChoice = getComputerChoice();
     const result = playRound(playerChoice, computerChoice);
     alert(result);
-    console.log(`Round ${i + 1}: ${result}`);
+    console.log(`%cRound ${i + 1}: ${result}`, "font-size:20px");
   }
 
   // Declare the winner
-  console.log("Player Score:", playerScore);
-  console.log("Computer Score:", computerScore);
+  console.log("%cPlayer Score:", PLAYER_LETTER, playerScore);
+  console.log("%cComputer Score:", COMPUTER_LETTER, computerScore);
   if (playerScore > computerScore) {
-    console.log("Congratulations! You won the game!");
+    console.log("%cCongratulations! You won the game!", "font-size:35px");
   } else if (playerScore < computerScore) {
-    console.log("Sorry! The computer won the game!");
+    console.log("%cOH NO! The computer won the game!", "font-size:35px");
   } else {
-    console.log("The game is a draw!");
+    console.log("%cThe game is a draw!", "font-size:35px");
   }
 }
 
